7a2adf92ac9493ca163ca5b1ac38eab3ce01cb61
rafael curi leonardo <rafaelcuri@cbpf.br>
2023-11-17 15:56:03 -0300

    card projetos

--- components/projetoCard.vue
+++ components/projetoCard.vue
@@ -22,7 +22,8 @@ const isMobile = useMediaQuery('(max-width: 780px)')
 
 					<NuxtLink :to="item.url" external target="_blank">
 						<img :src="item.img" :alt="item.descricao" class="hover:brightness-50 w-full h-auto ml-0 sm:ml-9 mt-5 rounded-2xl shadow-2xl" @mouseover="!isMobile ? titulo = false : ''" @mouseleave="titulo = true">
-						<p class="p-1 text-xl font-bold text-center ml-10  " :class="{ hidden: titulo }">{{ item.titulo }} </p>
+						<p v-if="!isMobile" class="p-1 text-xl font-bold text-center   " :class="{ hidden: titulo }">{{ item.titulo }} </p>
+						<p v-else class="p-1 text-xl font-bold text-center"> {{ item.titulo }} </p>
 					</NuxtLink>
 				</div>
 

21142b240b868fd9ad09312b2d197a4632a46dc8
rafaelcuri <rafaelcuri@cbpf.br>
2025-02-25 15:03:59 -0300

    ✨ melhorado design

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,15 +19,6 @@ const stacks = [
   { title: 'CSS', icon: 'devicon:css3', color: '#1572B6' },
   { title: 'Javascript', icon: 'devicon:javascript', color: '#F7DF1E' },
 ]
-
-const contact = [
-  { title: 'Github', icon: 'fa-brands:github', link: 'https://github/' },
-  { title: 'Linkedin', icon: 'fa-brands:linkedin', link: 'https://www.linkedin.com/in/bruno-oliveira-4b1b3b1b3/' },
-  { title: 'Email', icon: 'fa-solid:envelope', link: 'mailto:rafael@curi.dev.br' },
-  { title: 'Instagram', icon: 'fa-brands:instagram', link: 'https://www.instagram.com/bruno.oliveira.dev/' },
-  { title: 'Facebook', icon: 'fa-brands:facebook', link: 'https://www.facebook.com/bruno.oliveira.dev' },
-  { title: 'Youtube', icon: 'fa-brands:youtube', link: 'https://www.youtube.com/channel/UCJ1J6j7Z1J7ZJQJ1J6j7Z1J7ZJQ' },
-]
 </script>
 
 <template>
@@ -51,22 +42,6 @@ const contact = [
               <Icon :name="item.icon" :size="isMobile? 20 : 35" />
               <span>{{ item.title }}</span>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- TODO: AJEITAR RESPONSIVIDADE E LINKS DO CONTATO -->
-    <div class="fixed inset-x-0 bottom-4 mb-5">
-      <div class="flex items-center justify-center space-x-8">
-        <div v-for="({ icon, link, title }, idx) in contact" :key="idx" class="group relative">
-          <NuxtLink :to="link" target="_blank" :aria-label="title" class="transition-all duration-300 hover:drop-shadow-[0_0_5px_#F28AA9]">
-            <Icon :name="icon" :size="isMobile ? '2rem' : '3rem'" class="text-white transition-all duration-300 hover:text-blue-300" />
-          </NuxtLink>
-          <div class="absolute bottom-full left-1/2 mb-2 hidden -translate-x-1/2 group-hover:block">
-            <span class="whitespace-nowrap rounded bg-white px-2 py-1 text-xs text-black shadow-md">
-              {{ title }}
-            </span>
           </div>
         </div>
       </div>

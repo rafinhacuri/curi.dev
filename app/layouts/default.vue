d6ae9a4278ef3333a2eef65aa7fa0e5d62b4ea1f
Rafael Curi Leonardo <rafinhacurig@gmail.com>
2025-12-09 16:55:06 -0300

    ♻️feito pagina de projetos

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="pointer-events-none fixed inset-0 -z-10">
-    <div class="fixed top-0 left-0 h-64 w-64 rounded-full bg-emerald-500/15 blur-[100px]" />
-    <div class="fixed top-32 right-0 h-72 w-72 rounded-full bg-sky-400/10 blur-[100px]" />
-    <div class="fixed top-150 left-0 h-72 w-72 rounded-full bg-gray-400/6 blur-[100px]" />
-  </div>
   <Header />
   <UMain>
+    <div class="pointer-events-none fixed inset-0 -z-10">
+      <div class="fixed top-0 left-0 h-64 w-64 rounded-full bg-emerald-500/15 blur-[100px]" />
+      <div class="fixed top-32 right-0 h-72 w-72 rounded-full bg-sky-400/10 blur-[100px]" />
+      <div class="fixed bottom-0 left-0 h-72 w-72 rounded-full bg-gray-400/6 blur-[100px]" />
+    </div>
     <slot />
   </UMain>
   <Footer />
